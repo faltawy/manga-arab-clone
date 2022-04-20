5d4589afb478bee31e73ccf4f69987b53eb3bd22
Ahmed Hassan <76843311+ah7255703@users.noreply.github.com>
2022-04-20 21:34:07 +0200

    f

--- manga/manga_arab/getters.py
+++ manga/manga_arab/getters.py
@@ -2,9 +2,9 @@ from .utils import MangaArabApi
 from typing import List
 from .models import AnimeManga
 from .exceptions import (ConnectionError,NoResults)
-mangapi = MangaArabApi
 from httpx import AsyncClient
 
+mangapi = MangaArabApi
 class Getter:
     def __init__(self,session:AsyncClient):
         self.session = session
@@ -43,45 +43,3 @@ class Getter:
             return chapter_data.get('pages_url')
         except:
             NoResults(anime_slug + str(chapter))
-
-
-
-
-# async def search(search_term:str) -> List[AnimeManga]:
-#     querystring = {"name":str(search_term).lower(),"API_key":mangapi.API_key}
-#     async with ClientSession() as session:
-#         response = await session.get(mangapi.get_endpoint('search'),params=querystring)
-#         if not response.ok:
-#             raise ConnectionError
-#         else:
-#             resp_data = await response.json()
-#             if len(resp_data['data']) == 0:
-#                 raise NoResults(search_term)
-#             else:
-#                 results = [AnimeManga(**d) for d in resp_data['data']]
-                
-#                 return results
-
-
-# async def get_details(anime_slug)-> AnimeManga:
-#     url = mangapi.get_endpoint('manga-info')%(anime_slug)
-#     querystring = {"API_key":mangapi.API_key}
-#     response = await get(url,params=querystring)
-#     _details = await response.json()
-#     try:
-#         __details  = await _details.get('data')
-#         details = await __details.get('infoManga')[0]
-#         return AnimeManga(**details)
-#     except:
-#         raise NoResults(anime_slug)
-
-
-# async def read_chapter(anime_slug:str,chapter:int):
-#     querystring = {"API_key":mangapi.API_key}
-#     url = mangapi.get_endpoint('read-chapter')%(anime_slug,str(chapter))
-#     try:
-#         response = await get(url,params=querystring)
-#     except:
-#         NoResults(anime_slug + str(chapter))
-#     chapter_data =await response.json()
-#     return chapter_data.get('pages_url')
